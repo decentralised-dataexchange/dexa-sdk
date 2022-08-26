cd3edab02729afc1483885ce2593e175f3b4f2ed
George J Padayatti <george.padayatti@igrant.io>
2022-08-27 01:33:21 +0530

    Configure logger and jsonld cache for the package on import
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- dexa_sdk/__init__.py
+++ dexa_sdk/__init__.py
@@ -1,4 +1,8 @@
-from .logs import configure_logger
+from .logs.core import configure_logger
+from .jsonld.core import configure_jsonld_simple_cache
 
 # Configure loguru logger
 configure_logger()
+
+# Configure in-memory cache for jsonld instances
+configure_jsonld_simple_cache()
