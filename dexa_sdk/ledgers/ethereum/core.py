ae458d068c24903688539c54d67f10a3ee45591b
George J Padayatti <george.padayatti@igrant.io>
2022-09-06 16:18:24 +0530

    Add ledger functions for Indy, Ethereum
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

---
+++ dexa_sdk/ledgers/ethereum/core.py
@@ -0,0 +1,234 @@
+import typing
+import asyncio
+from web3 import Web3, Account
+from web3.contract import Contract
+from web3.exceptions import ContractLogicError
+from web3._utils.encoding import to_json
+from eth_account.signers.local import LocalAccount
+from aries_cloudagent.config.injection_context import InjectionContext
+from loguru import logger
+
+
+class EthereumClient:
+    """Ethereum blockchain client"""
+
+    def __init__(self, context: InjectionContext) -> None:
+        """Initialise ethreum blockchain client"""
+
+        # Injection context
+        self._context = context
+
+        # Logger
+        self._logger = logger
+
+        # Eth node rpc endpoint
+        self._eth_node_rpc = self._context.settings.get("dexa.eth_node_rpc")
+
+        # Eth private key of the controller
+        self._org_eth_private_key = self._context.settings.get("dexa.org_eth_private_key")
+
+        # Set organisation account for ethereum client
+        self._org_eth_account = Account.from_key(self._org_eth_private_key)
+
+        # Eth private key of the intermediary
+        self._intermediary_eth_private_key = self._context.settings.get(
+            "dexa.intermediary_eth_private_key")
+
+        # Set intermediary account for ethereum client
+        self._intermediary_eth_account = Account.from_key(self._intermediary_eth_private_key)
+
+        # Ethereum client
+        self._client = Web3(Web3.HTTPProvider(self._eth_node_rpc))
+
+        # Contract ABI
+        self._contract_abi = self._context.settings.get("dexa.contract_abi")
+
+        # Contract address
+        self._contract_address = self._context.settings.get("dexa.contract_address")
+
+        # Contract interface
+        self._contract: Contract = self._client.eth.contract(
+            address=self._contract_address,
+            abi=self._contract_abi
+        )
+
+    @property
+    def context(self) -> InjectionContext:
+        """Accessor for injection context.
+
+        Returns:
+            InjectionContext: Injection context
+        """
+        return self._context
+
+    @property
+    def logger(self):
+        """Accessor for logger.
+
+        Returns:
+            Logger: Logger
+        """
+        return self._logger
+
+    @property
+    def client(self) -> Web3:
+        """Returns the client for interacting with ethereum blockchain node.
+
+        Returns:
+            Web3: Ethereum blockchain client
+        """
+        return self._client
+
+    @property
+    def org_account(self) -> LocalAccount:
+        """Accessor for ethereum account for organisation.
+
+        Returns:
+            LocalAccount: account
+        """
+        return self._org_eth_account
+
+    @property
+    def org_private_key(self) -> str:
+        """Accessor for organisation private key.
+
+        Returns:
+            str: private key
+        """
+        return self._org_eth_private_key
+
+    @property
+    def intermediary_account(self) -> LocalAccount:
+        """Accessor for ethereum account for intermediary.
+
+        Returns:
+            LocalAccount: account
+        """
+        return self._intermediary_eth_account
+
+    @property
+    def intermediary_private_key(self) -> str:
+        """Accessor for intermediary private key.
+
+        Returns:
+            str: private key
+        """
+        return self._intermediary_eth_private_key
+
+    @property
+    def contract_abi(self) -> str:
+        """Accessor for dexa contract abi.
+
+        Returns:
+            str: smart contract abi
+        """
+        return self._contract_abi
+
+    @property
+    def contract_address(self) -> str:
+        """Accessor for dexa contract address.
+
+        Returns:
+            str: smart contract address
+        """
+        return self._contract_address
+
+    @property
+    def contract(self) -> Contract:
+        """Accessor for dexa contract interface.
+
+        Returns:
+            Contract: smart contract
+        """
+        return self._contract
+
+    async def emit_da_did(self, did: str) -> typing.Tuple[typing.Any, typing.Any]:
+        """Emit did:mydata identifier in the blockchain logs"""
+        org_account = self.org_account
+        org_balance = self.client.eth.get_balance(org_account.address)
+
+        self.logger.info(f"Organisation account address: {org_account.address}")
+        self.logger.info(f"Organisation account balance: {org_balance}")
+
+        try:
+            contract = self.contract
+            contract_function = contract.functions.emitDADID(did)
+            contract_function_txn = contract_function.buildTransaction(
+                {
+                    'from': org_account.address,
+                    'nonce': self.client.eth.get_transaction_count(org_account.address),
+                    'maxFeePerGas': 2000000000,
+                    'maxPriorityFeePerGas': 1000000000
+                }
+            )
+
+            tx_create = self.client.eth.account.sign_transaction(
+                contract_function_txn,
+                org_account.privateKey
+            )
+
+            tx_hash = self.client.eth.send_raw_transaction(tx_create.rawTransaction)
+
+            # Suspend execution and let other task run.
+            await asyncio.sleep(5)
+
+            tx_receipt = self.client.eth.wait_for_transaction_receipt(tx_hash)
+
+            if tx_receipt.get("status") == 1:
+                self.logger.info(f"Status (emitDADID): Succesfully emitted {did}")
+            else:
+                self.logger.info(
+                    "Status (emitDADID): Failed to emit {did}")
+
+            return (tx_hash, tx_receipt)
+        except ContractLogicError as err:
+            self.logger.info(f"Status (emitDADID): {err}")
+
+    async def add_organisation(self) -> None:
+        """Add organisation to the whitelist"""
+        org_account = self.org_account
+        org_balance = self.client.eth.get_balance(org_account.address)
+
+        self.logger.info(f"Organisation account address: {org_account.address}")
+        self.logger.info(f"Organisation account balance: {org_balance}")
+
+        intermediary_account = self.intermediary_account
+        intermediary_balance = self.client.eth.get_balance(intermediary_account.address)
+
+        self.logger.info(f"Intermediary account address: {intermediary_account.address}")
+        self.logger.info(f"Intermediary account balance: {intermediary_balance}")
+        self.logger.info(f"Transaction count: \
+                {self.client.eth.get_transaction_count(intermediary_account.address)}")
+
+        try:
+            contract = self.contract
+            contract_function = contract.functions.addOrganisation(org_account.address)
+            contract_function_txn = contract_function.buildTransaction(
+                {
+                    'from': intermediary_account.address,
+                    'nonce': self.client.eth.get_transaction_count(intermediary_account.address),
+                    'maxFeePerGas': 2000000000,
+                    'maxPriorityFeePerGas': 1000000000
+                }
+            )
+
+            tx_create = self.client.eth.account.sign_transaction(
+                contract_function_txn,
+                intermediary_account.privateKey
+            )
+
+            tx_hash = self.client.eth.send_raw_transaction(tx_create.rawTransaction)
+
+            self.logger.info(f"Transaction hash (addOrganisation): {tx_hash.hex()}")
+
+            tx_receipt = self.client.eth.wait_for_transaction_receipt(tx_hash)
+
+            self.logger.info(f"Transaction receipt (addOrganisation): {to_json(tx_receipt)}")
+
+            if tx_receipt.get("status") == 1:
+                self.logger.info("Status (addOrganisation): Added organisation to whitelist.")
+            else:
+                self.logger.info(
+                    "Status (addOrganisation): Organisation is already present in whitelist.")
+        except ContractLogicError as err:
+            self.logger.info(f"Status (addOrganisation): {err}")
---
+++ dexa_sdk/ledgers/indy/core.py
@@ -0,0 +1,105 @@
+import typing
+from asyncio import shield
+from aries_cloudagent.ledger.base import BaseLedger
+from aries_cloudagent.issuer.base import BaseIssuer
+from aries_cloudagent.config.injection_context import InjectionContext
+
+
+class IndyLegerConfigError(Exception):
+    """Raised when ledger is misconfigured"""
+
+
+async def create_schema_def_and_anchor_to_ledger(
+    context: InjectionContext,
+    schema_name: str,
+    schema_version: str,
+    attributes: typing.List[str]
+) -> typing.Tuple[str, dict]:
+    """
+    Create scheme definition and anchor to ledger.
+
+    Args:
+        schema_name: Schema name.
+        schema_version: Schema version.
+        attributes: List of attributes.
+
+    :return: (schema_id, schema_def)
+    """
+
+    # Ledger instance from context
+    ledger: BaseLedger = await context.inject(BaseLedger, required=False)
+    if not ledger:
+        reason = "No ledger available"
+        if not context.settings.get_value("wallet.type"):
+            reason += ": missing wallet-type?"
+
+        raise IndyLegerConfigError(
+            f"{reason}"
+        )
+
+    # Issuer instance from context
+    issuer: BaseIssuer = await context.inject(BaseIssuer)
+
+    async with ledger:
+        # Create schema
+        schema_name = schema_name
+        schema_version = schema_version
+        attributes = attributes
+
+        schema_id, schema_def = await shield(
+            ledger.create_and_send_schema(
+                issuer, schema_name, schema_version, attributes
+            )
+        )
+
+        return schema_id, schema_def
+
+
+async def create_cred_def_and_anchor_to_ledger(
+    context: InjectionContext,
+    schema_id: str,
+    tag: str = "default",
+    support_revocation: bool = False
+) -> typing.Tuple[str, dict, bool]:
+    """
+    Create credential definition and anchor to ledger.
+
+    Args:
+        schema_id: Schema id.
+        tag: Tag.
+        support_revocation: Support revocation.
+
+    Returns:
+        :rtype: tuple: (credential definition id, credential definition json, support revocation)
+    """
+
+    # Ledger instance from context
+    ledger: BaseLedger = await context.inject(BaseLedger, required=False)
+    if not ledger:
+        reason = "No ledger available"
+        if not context.settings.get_value("wallet.type"):
+            reason += ": missing wallet-type?"
+
+        raise IndyLegerConfigError(
+            f"{reason}"
+        )
+
+    # Issuer instance from context
+    issuer: BaseIssuer = await context.inject(BaseIssuer)
+
+    tag = "default"
+    support_revocation = False
+
+    async with ledger:
+        # Create credential definition
+        (cred_def_id, cred_def, novel) = await shield(
+            ledger.create_and_send_credential_definition(
+                issuer,
+                schema_id,
+                signature_type=None,
+                tag=tag,
+                support_revocation=support_revocation,
+            )
+        )
+
+        return cred_def_id, cred_def, novel
